a80c5f13e6fa612b877392033d7986d09fba4c56
Mehdi Filban <137497223+mehdi-filban@users.noreply.github.com>
2026-02-16 13:54:28 +0330

    About Section

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,5 +1,4 @@
 import { motion } from "framer-motion";
-import profile from "../assets/mehdi.jpeg"; 
 
 export default function About() {
   return (
@@ -24,7 +23,7 @@ export default function About() {
             />
 
             <img
-              src={profile}
+              src="/mehdi.jpeg"
               alt="Mehdi Filban"
               className="relative w-56 h-56 sm:w-64 sm:h-64 rounded-full object-cover shadow-xl ring-4 ring-white"
             />
